d6993ddeebdbf18bdfd7950dcc6ca5d72b238cad
alentheguy <123839330+alentheguy@users.noreply.github.com>
2024-02-02 13:43:19 -0600

    made methods

--- QuestionsGame.java
+++ QuestionsGame.java
@@ -3,10 +3,29 @@
 // You should delete this comment and replace it with your class
 // header comment.
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class QuestionsGame {
     // Your code here
 
     private static class QuestionNode {
         // Your code here
+    }
+
+    public QuestionsGame(String object) {
+
+    }
+
+    public QuestionsGame(Scanner input) {
+
+    }
+
+    public void saveQuestions(PrintStream output) {
+
+    }
+
+    public void play() {
+        
     }
 }
